4d6c9c161fcc26dd63dc835f00e6ceaa62fcc013
160512 <sjt409768982@hotmail.com>
2019-06-12 20:10:40 +0800

    1.beta test

--- javascript/index_javascript.js
+++ javascript/index_javascript.js
@@ -32,8 +32,14 @@ function setHtmlHaderTime(oNowDate) {
     //输出信息
     $('.time').text('当前时间' + iNowYear + '年' + iNowMonth + '月' + iNowDay + '日' + aWeekday[iNowWeek] + ' ' + anyNowHour + ':' + anyNowMinute + ':' + anyNowSecond);
 }
-//设置当前周数
+//设置当前周次
 $(document).ready(function setHtmlHaderWeek() {
+    iCutWeeks = getStudyWeeks();
+    $('.week').text('本学期第' + iCutWeeks + '周');
+});
+
+//获取当前学期周次
+function getStudyWeeks() {
     var oNowDate = new Date();
     var oStartDate = isTimeInTheSemester(oNowDate).StartDate;//学期起始时间
     var iMonth = oStartDate.getMonth() - 1;
@@ -41,8 +47,9 @@ $(document).ready(function setHtmlHaderWeek() {
     var oCutDate = oNowDate - oStartDate;//实际日期差
     var iCutDay = Math.floor(oCutDate / (3600 * 24 * 1000));//转换天数
     var iCutWeeks = parseInt(iCutDay / 7) + 1;//计算差日期
-    $('.week').text('本学期第' + iCutWeeks + '周');
-});
+    return iCutWeeks;
+}
+
 //判断是否在学期内并返回学期期间
 function isTimeInTheSemester(oNowDate) {
     var oWinterStartDate = new Date(2019, 2, 18);//第二学期开始时间
@@ -66,8 +73,14 @@ function isTimeInTheSemester(oNowDate) {
 //加载课程表
 $(document).ready(function loadCurriculumXML() {
     var iTimeFadeIn = 1000;
+    var iCutWeeks = getStudyWeeks();
+    if (iCutWeeks % 2 == 0) {
+        bDoubleWeek = true;
+    } else {
+        bDoubleWeek = false;
+    }
     $.ajax({
-        url: 'https://160512.github.io/MyLoveWeb/XML/Curriculum.xml',//发送请求的地址
+        url: 'https://160512.github.io/ComprehensiveWebsiteForLove/xml/Curriculum.xml',//发送请求的地址
         dataType: 'xml',//预期服务器返回的数据类型
         type: 'GET', //请求方式
         timeout: 2000,//设置请求超时时间
@@ -77,49 +90,36 @@ $(document).ready(function loadCurriculumXML() {
         success: function (xml) {//请求成功后的回调函数
             $(xml).find('Week').each(function (i) {//查找所有Week节点并遍历
                 var iWeekNumber = $(this).attr('week');//获取周次
-                $(this).find('class').each(function (j) {//查找当前周次所有class节点并遍历
-
+                $(this).find('lesson').each(function (j) {//查找当前周次所有class节点并遍历
                     //var class_id = $(this).children('class');//获得子节点
                     var iClassNumber = $(this).attr('class');//获取节次
                     var iStartWeekNumber = $(this).attr('startWeek');//获取开始周次
                     var iEndWeekNumber = $(this).attr('endWeek');//获取结束周次
-                    var sOoTSwitch = $(this).attr('OoT');//获取单双周或者全周
+                    var sOoT = $(this).attr('OoT');//获取单双周或者全周
                     var sRoom = $(this).attr('room');//获取教室
                     var sClassName = $(this).text();//获取课程
 
-                    var sOoTText;
-                    switch (sOoTSwitch) {//获取修改单双周
-                        case 'O':
-                            sOoTText = '单周';
-                            break;
-                        case 'T':
-                            sOoTText = '双周';
-                            break;
-                        case 'A':
-                            sOoTText = '周';
-                            break;
-                        case 'L':
-                            sOoTText = '临时';
-                            break;
+                    if (iStartWeekNumber <= iCutWeeks && iCutWeeks <= iEndWeekNumber) {
+                        var bDisplayWeek = true;//在周次中
+                    } else {
+                        var bDisplayWeek = false;//不在周次中
+                    }
+                    if ((bDoubleWeek == true && sOoT == '单周') || (bDoubleWeek == false && sOoT == '双周')) {
+                        bDisplayWeek = false;
                     }
 
-                    //制作标签
-                    var sClassTag = getClassTag(iWeekNumber, iClassNumber);
-                    var sRoomTag = getRoomTag(iWeekNumber, iClassNumber);
 
-                    if (sClassName == 'NULL') {//没有课程
-                        $(sClassTag).text(sClassName);
-                        $(sRoomTag).text(sRoom);
-                    } else {//有课程
-                        //e.g.<p id='class25'>选修食品<span class='startweek'>4</span>-<span class='endweek'>12</span>周<p id='room25'>@北阶104</td>
-                        $(sClassTag).html(sClassName + '<span class=\"startweek\">' + iStartWeekNumber + '</span>-<span class=\"endweek\">' + iEndWeekNumber + '</span>' + sOoTText);
-                        $(sRoomTag).text('@' + sRoom);
 
-                        $(sClassTag).fadeIn(iTimeFadeIn);
-                        $(sRoomTag).fadeIn(iTimeFadeIn);
+                    if (sClassName != 'NULL' && bDisplayWeek == true) {//显示有课程表格
+                        var sClassDataTag = '#classdata' + iWeekNumber + iClassNumber;//制作标签
+                        var sClassDataHtml = $(sClassDataTag).html();//获取标签html内容
+                        //新增html内容
+                        var sClassData = sClassDataHtml + '<div class="classdetails"><p>' + sClassName + '<span>' + iStartWeekNumber + '-' + iEndWeekNumber + '</span><span>' + sOoT + '</span></p><p>' + sRoom + '</div></p>';
+                        $(sClassDataTag).html(sClassData);//修改新增内容
+                        console.log(sClassDataHtml);
+                        $(sClassDataTag).children('.classdetails').css('display', 'none');
+                        $(sClassDataTag).children('.classdetails').fadeIn(iTimeFadeIn);
                         iTimeFadeIn = iTimeFadeIn + 200;
-                        //$(sClassTag).css('display', 'block');//显示列表
-                        //$(sRoomTag).css('display', 'block');//显示列表
                     }
                 });
             });
